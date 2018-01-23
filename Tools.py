794a2c04088ffd7881a78112495b8f656dae9204
stevenang <stevenkhoang@me.com>
2018-01-22 21:03:52 -0500

    Modified string_to_binary() method

--- Tools.py
+++ Tools.py
@@ -4,6 +4,17 @@ class Tools:
     def string_to_binary(input:str):
         binary = []
         for char in input:
+            temp = bin(ord(char))[2:]
+            while(len(temp) < 8):
+                temp = '0' + temp
+            binary.append(temp)
+
+        return ''.join(binary)
+
+    @staticmethod
+    def string_to_binary_no_concat(input: str):
+        binary = []
+        for char in input:
             binary.append(bin(ord(char))[2:])
 
         return ''.join(binary)
